7282db1a4455adcbadf021b01d9fdf39db9de224
Shikhar Kesarwani <Shikharkesarwani2006@gmail.com>
2026-02-22 17:50:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9606050239-a2862",
+  "appId": "1:72073274056:web:fbb3d9c741fda65a4dbbfd",
+  "apiKey": "AIzaSyCHOhoRoKBIF-5v1UeYVByzc-umDGwQuL8",
+  "authDomain": "studio-9606050239-a2862.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "72073274056"
+};
