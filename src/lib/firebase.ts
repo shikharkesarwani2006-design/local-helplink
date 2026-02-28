7593c971865fd620ecce55b32c4c5c13027b037c
Shikhar Kesarwani <Shikharkesarwani2006@gmail.com>
2026-02-28 09:26:46 +0000

    sign in issue check api key-AIzaSyD9pyiJbJMgPrUmn52l1ZtLh2x9Xl6ZbwQ

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBz1mYFXYLUlZCEcKGPxj0xo09RQNLbErw",
+  authDomain: "local-helplink.firebaseapp.com",
+  projectId: "local-helplink",
+  storageBucket: "local-helplink.firebasestorage.app",
+  messagingSenderId: "1075835588588",
+  appId: "1:1075835588588:web:98cee886e701d75aa2bac9",
+  measurementId: "G-ZZJW7BN09R"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
